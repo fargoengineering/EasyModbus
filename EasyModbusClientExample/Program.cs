ff803e1b4c5801f654fe69f39d6290a6c1657d43
fargoengineering <dsmith@fargoengineering.com>
2024-03-05 10:29:11 -0600

    example code in program.cs

--- EasyModbusClientExample/Program.cs
+++ EasyModbusClientExample/Program.cs
@@ -38,41 +38,75 @@ namespace EasyModbusClientExample
 		[STAThread]
 		private static void Main(string[] args)
 		{
+			// EasyMODBUS Example
 			//Application.EnableVisualStyles();
 			//Application.SetCompatibleTextRenderingDefault(false);
 			//Application.Run(new MainForm());
 
+
+			// FEI MODBUS CODE
+
+			// Initialize modbus class
 			Graco proDispense = new Graco();
 
+			// Connect to IP / Port
 			Console.Write("Connected: ");
 			Console.WriteLine(proDispense.Connect("172.16.72.12", 502));
 
+			// Set System state 2 = Standby On
 			proDispense.setSystemState(2);
 
+			// Confirm System State
 			Console.Write("System State set to: ");
 			Console.WriteLine(proDispense.getSystemState());
 
+			// Set Recipe
 			proDispense.setRecipe(50);
-            //proDispense.setRecipe(16);
 
+			// Confirm Recipe
             Console.Write("Current Recipe set to: ");
 			Console.WriteLine(proDispense.getRecipe());
 
+			// Get Target fills for each port
 			int[] targets = proDispense.getDispenseTargets();
 			Console.Write("Fill Target on port 1 = ");
 			Console.WriteLine(targets[0]);
             Console.Write("Fill Target on port 2 = ");
-            Console.WriteLine(targets[2]);
+            Console.WriteLine(targets[1]);
             Console.Write("Fill Target on port 3 = ");
-            Console.WriteLine(targets[4]);
+            Console.WriteLine(targets[2]);
             Console.Write("Fill Target on port 4 = ");
-            Console.WriteLine(targets[6]);
+            Console.WriteLine(targets[3]);
 
-            Console.Write("Current Fill Value");
+			// Get flow cc/min for each port
 			int[] fills = proDispense.getCurrentFlow();
+            Console.Write("Current Fill Value on port 1 = ");
 			Console.WriteLine(fills[0]);
+			Console.Write("Current Fill value on port 2 = ");
+			Console.WriteLine(fills[1]);
+            Console.Write("Current Fill value on port 3 = ");
+            Console.WriteLine(fills[2]);
+            Console.Write("Current Fill value on port 4 = ");
+            Console.WriteLine(fills[3]);
 
-			proDispense.disconnect();
+			// Get Current state for each port
+			int[] states = proDispense.getPortState();
+			Console.Write("Current port 1 state: ");
+			Console.WriteLine(states[0]);
+            Console.Write("Current port 2 state: ");
+            Console.WriteLine(states[1]);
+            Console.Write("Current port 3 state: ");
+            Console.WriteLine(states[2]);
+            Console.Write("Current port 4 state: ");
+            Console.WriteLine(states[3]);
+
+			// Example: Start Fill and Monitor
+			// proDispense.setSystemState(3);	// Set state to DISPENSE
+			// proDispense.getCurrentFlow();	// Monitor Flow Rate
+			// proDispense.getCurrentVolume();	// Monitor Job Volume
+
+			// Disconnect from MODBUS
+            proDispense.disconnect();
 
 		}
 		
